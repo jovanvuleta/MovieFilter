f0157c1a2b4a91966294b52391cb729baa52582b
jocavuleta <jocavuleta@email.com>
2020-02-16 14:49:10 +0100

    Final commit

---
+++ main.js
@@ -0,0 +1,38 @@
+// var parent = document.querySelector(".parent");
+
+// parent.addEventListener("mousemove", function doSomething(e) {
+//   console.log(e);
+// });
+
+// var input = document.querySelector("input");
+
+// input.addEventListener("keyup", function doSomething(e) {
+//   console.log(e.target.value);
+// });
+
+// var body = document.querySelector("body");
+// var player = document.querySelector(".player");
+
+// function movePlayer(e) {
+//   player.style.left = e.clientX + "px";
+//   player.style.top = e.clientY + "px";
+// }
+
+// body.addEventListener("mousemove", movePlayer);
+
+// var parent = document.querySelector(".parent");
+// var child = document.querySelector(".child");
+
+// parent.addEventListener("click", function(e) {
+//   console.log("Parent");
+// });
+// child.addEventListener("click", function(e) {
+//   console.log("Child");
+//   e.stopPropagation();
+// });
+
+// var link = document.querySelector("a");
+
+// link.addEventListener("click", function(e) {
+//   e.preventDefault();
+// });
